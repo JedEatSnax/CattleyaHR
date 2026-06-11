d46d73f5e7a6a6b21d8aac80589b2a6c522b5ab3
JedEatSnax <134795258+JedEatSnax@users.noreply.github.com>
2026-06-11 18:10:45 +0800

    chore(client): add shadcn login and signup blocks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,24 @@
 import { lazy, Suspense } from "react"
 import { Skeleton } from "@/components/ui/skeleton"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import ErrorBoundary from "@/components/error-boundary"
 
-const Login = lazy(() => import("./pages/Login"))
-const Registration = lazy(() => import("./pages/Registration"))
+const Login = lazy(() => import("@/pages/Login"))
+const Registration = lazy(() => import("@/pages/Registration"))
 
 export function App() {
   return (
-    <Suspense fallback={<Skeleton />}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/registration" element={<Registration />} />
-        </Routes>
-      </BrowserRouter>
-    </Suspense>
+    <ErrorBoundary>
+      <Suspense fallback={<Skeleton />}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registration" element={<Registration />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </ErrorBoundary>
   )
 }
 
