02d9576ae01d0d3711af2bc88f65cc629183c78e
JedEatSnax <134795258+JedEatSnax@users.noreply.github.com>
2026-06-14 23:06:48 +0800

    chore(server): add openzeppelin package and revise eslint config

---
+++ server/contracts/Test.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity ^0.8.28;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ExampleToken is ERC20 {
+  constructor() ERC20("ExampleToken", "ETK") {}
+}
